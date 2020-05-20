a4047b8a356c7221f43fbaf689c0f9720a4a01d6
Danila Odintsov <captain-kush@yandex.ru>
2020-05-20 13:45:17 +0400

    Make a second mode

--- src/SliderPage.js
+++ src/SliderPage.js
@@ -1,7 +1,42 @@
 import React, {Component} from "react";
 import {Link} from "react-router-dom";
 import Carousel from 'react-elastic-carousel';
+import { createStore } from 'redux';
 
+
+let xhr = new XMLHttpRequest(), files = [];
+const store = createStore(filesReducer, files);
+xhr.open('GET', 'http://testimages.osora.ru:86', true);
+xhr.send();
+xhr.onload = function() {
+    files = xhr.responseText;
+    files = files.split('').filter(s => s !== '"');
+    files.splice(0, 1);
+    files.splice(files.length - 1, 1);
+    files = files.join('').split(',');
+    
+    for (let i = 0; i < files.length; i++) {
+        store.dispatch({
+            type: 'ADD_FILE',
+            id: i + 1,
+            src: files[i]
+        })
+    }
+}
+function filesReducer(state, action) {
+    switch (action.type) {
+        case 'ADD_FILE':
+            return [
+                ...state,
+                {
+                    id: action.id,
+                    src: action.src
+                }
+            ]
+        default:
+            return state
+    }
+}
 
 export default class SliderPage extends Component {
     state = {
@@ -12,13 +47,15 @@ export default class SliderPage extends Component {
           {id: 4, src: 'images/img4.jpg'},
           {id: 5, src: 'images/img5.jpg'}
         ]
-    }
+    };
+
     render() {
-      const { items } = this.state;
+      let { items } = this.state;
       return (
           <div>
             <h2>Слайдер</h2>
             <Link to="/">Назад</Link>
+            <button>Change</button>
             <Carousel className='slider'>
                  {items.map(item => <div key={item.id} className="slider_item"><img className='img' src={item.src} /></div>)}
             </Carousel>
